d22b69c0a36e8de0284673355ba98dbc30e580f8
Florian Fortier <78166457+FlorianFortier@users.noreply.github.com>
2024-12-05 11:01:53 +0100

    changing tomcat port

--- init.sql
+++ init.sql
@@ -1,2 +1,2 @@
-INSERT INTO users (id, password, role, username) VALUES (1, 'password1', 'Organizer', 'organizer_user');
-INSERT INTO users (id, password, role, username) VALUES (2, 'password2', 'Praticien', 'praticien_user');
+INSERT INTO users (id, password, role, username) VALUES (1, '$2a$10$D8VDGfqB3tMPZ6pCFFvb8O92PbeiGAaKL2n4K9zmjb1M/zaHhGpE6', 'Organizer', 'organizer_user');
+INSERT INTO users (id, password, role, username) VALUES (2, '$2a$10$Fq/ElxqY5nJtwCd9ifPgrunXXVkOctlDcTRI7bTVlK1.UXVuZFWfi', 'Praticien', 'praticien_user');
